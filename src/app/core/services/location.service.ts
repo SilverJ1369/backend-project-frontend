5c1460bc543611d743d65a8d096f0a517ecd9f37
SilverJ1369 <35865016+SilverJ1369@users.noreply.github.com>
2024-03-11 19:44:20 -0500

    Refactor location service and main topic form component

--- src/app/core/services/location.service.ts
+++ src/app/core/services/location.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Location } from '../../shared/models/location';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getLocations() {
+    return this.http.get(`${environment.apiUrl}/locations`);
+  }
+
+  createLocation(data: any): Observable<any>{
+    console.log(data);
+
+    return this.http.post(`${environment.apiUrl}/locations`, data);
+  }
+
+  updateLocation(location: Location) {
+    return this.http.put(`${environment.apiUrl}/locations`, location);
+  }
+
+  deleteLocation(location: any) {
+    return this.http.delete(`${environment.apiUrl}/locations/${location.id}`);
+  }
 }
