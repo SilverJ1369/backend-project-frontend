56815dfe53bcb259cf6c9b2c2e98594eeebed426
SilverJ1369 <35865016+SilverJ1369@users.noreply.github.com>
2024-03-11 19:58:05 -0500

    Refactor event date service and modal component

--- src/app/core/services/event-date.service.ts
+++ src/app/core/services/event-date.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventDateService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getEventDates() {
+    return this.http.get(`${environment.apiUrl}/event-dates`);
+  }
+
+  createEventDate(data: any): Observable<any> {
+    return this.http.post(`${environment.apiUrl}/event-dates`, data);
+  }
+
+  updateEventDate(data: any) {
+    return this.http.put(`${environment.apiUrl}/event-dates`, data);
+  }
+
+  deleteEventDate(data: any) {
+    return this.http.delete(`${environment.apiUrl}/event-dates/${data.id}`);
+  }
 }
