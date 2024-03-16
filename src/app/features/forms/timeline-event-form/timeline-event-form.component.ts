4889e5ebe28ae4baf82115a1e0d865969eab6c0e
SilverJ1369 <35865016+SilverJ1369@users.noreply.github.com>
2024-03-16 12:47:02 -0500

    Add new routes and form for timeline event, removed unused imports

--- src/app/features/forms/timeline-event-form/timeline-event-form.component.ts
+++ src/app/features/forms/timeline-event-form/timeline-event-form.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MainTopic } from '../../../shared/models/main-topic';
+import { MainTopicService } from '../../../core/services/main-topic.service';
+import { ModalComponent } from '../../../shared/modal/modal.component';
+import { TimelineEventService } from '../../../core/services/timeline-event.service';
 
 @Component({
   selector: 'app-timeline-event-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, ModalComponent],
   templateUrl: './timeline-event-form.component.html',
   styleUrl: './timeline-event-form.component.scss'
 })
-export class TimelineEventFormComponent {
+export class TimelineEventFormComponent implements OnInit{
+
+  mainTopics: MainTopic[] = [];
+
+  @ViewChild(ModalComponent) modal!: ModalComponent;
+
+  @Input() locationID!: number;
+  @Input() eventDateID!: number;
+
+  timelineEventForm = new FormGroup({
+    main_topic_id: new FormControl(null, Validators.required),
+    location_id: new FormControl(0, Validators.required),
+    details: new FormControl(''),
+    event_date_id: new FormControl(0),
+    is_graphic: new FormControl(false),
+  });
+
+  constructor(private mainTopicService: MainTopicService, private timelineEventService: TimelineEventService) { }
+
+  ngOnInit(): void {
+    this.mainTopicService.getMainTopics().subscribe({
+      next: (mainTopics: MainTopic[]) => {
+        this.mainTopics = mainTopics;
+        console.log(this.mainTopics);
+      }
+    })
+  }
+
+  onSubmit() {
+    this.timelineEventForm.value.location_id = this.locationID;
+    this.timelineEventForm.value.event_date_id = this.eventDateID;
+    this.timelineEventService.createTimelineEvent(this.timelineEventForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+      }
+    })
+  }
+
+  openLocationModal() {
+    this.modal.openLocationDialog();
+  }
+
+  openEventDateModal() {
+    this.modal.openEventDateDialog();
+  }
 
 }
